1f140e74adeb812f78900bfc8f345e3ed194b163
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-09 16:34:30 +0100

    Toevoeging constantes, commentaar get prijs, implementatie getEenheidNaam methode

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,6 +1,14 @@
 package model;
 
 public abstract class Product {
+	//Constantes eenheden
+	public static final int GRAM = 1;
+	public static final int KILO = 2;
+	public static final int LITER = 3;
+	public static final int STUK = 4;
+	public static final int PAK = 5;
+	
+	
 	
 	//Constructoren
 	public Product(String naam, int prijspereenheid, String imageUrl, int eenheid){}
@@ -8,6 +16,9 @@ public abstract class Product {
 	
 	
 	//Concrete methodes
+	/*
+	 * Geeft de prijs voor hoeveelheid artikelen
+	 */
 	public int getPrijs(int hoeveelheid){	
 		return hoeveelheid * getPrijsPerEenheid();
 	}
@@ -21,6 +32,31 @@ public abstract class Product {
 		return "[" + getNaam() + ", " + getEenheid() + ", " + getPrijsPerEenheid() + "]";
 	}
 	
+	/*
+	 * Geeft een String representatie van de meegegeven eenheid
+	 */
+	public static String getEenheidNaam(int eenheid){
+		String result = "";
+		switch(eenheid){
+			case GRAM : 
+				result = "gram";
+				break;
+			case KILO :
+				result = "kilo";
+				break;
+			case LITER :
+				result = "liter";
+				break;
+			case STUK :
+				result = "stuk";
+				break;
+			case PAK :
+				result = "pak";
+				break;
+		}
+		
+		return result;
+	}
 	
 	
 	
