29107e516716aff595c447fd36026a1cb02695e2
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-11 15:25:59 +0100

    Image path vlinder aangepast, en path van index aangepast

--- src/webpagina/Index.java
+++ src/webpagina/Index.java
@@ -15,13 +15,13 @@ import model.Product;
 /**
  * Servlet implementation class Index
  */
-@WebServlet("/")
+@WebServlet("/index.html")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	private ArrayList<DefaultProduct> voorraad = new ArrayList<>();
-	{
-		voorraad.add(new DefaultProduct("Opgezette vlinder", 120, "images/atalanta.jpg", Product.STUK));
+	private static ArrayList<DefaultProduct> voorraad = new ArrayList<>();
+	static {
+		voorraad.add(new DefaultProduct("Opgezette vlinder", 120, "/DynamicWebshop/images/atalanta.jpg", Product.STUK));
 		voorraad.add(new DefaultProduct("Industriële bouwlamp", 160, "https://s.s-bol.com/imgbase0/imagebase/large/FC/9/0/8/3/9200000010483809.jpg", Product.STUK));
 	}
 
