4e5d58fbe8ee91e5ad45ce36e5b52729259f8893
Jenny <jenny.den.ouden@gmail.com>
2016-11-12 16:38:45 +0100

    Klasse Winkelwagen gemaakt

---
+++ src/bestel/Winkelwagen.java
@@ -0,0 +1,35 @@
+package bestel;
+
+import java.util.ArrayList;
+
+import model.Product;
+
+/*
+ * Klasse winkelwagen bevat alle bestellingen van de klant
+ */
+public class Winkelwagen {
+
+	//Instantievariabelen
+	private ArrayList<ProductHoeveelheid> bestellingen = new ArrayList<>();
+	
+	/*
+	 * Geeft de totale prijs van alle producten in de winkelwagen
+	 */
+	public int getTotaalPrijs(){
+		int result = 0;
+		for(ProductHoeveelheid ph : this.bestellingen){
+			result += ph.getPrijs();
+		}
+		
+		return result;
+	}
+	
+	/*
+	 * Voegt een nieuwe bestelregel toe, met hoeveelheid producten
+	 */
+	public void bestel(Product product, int hoeveelheid){
+		ProductHoeveelheid ph = new ProductHoeveelheid(product, hoeveelheid);
+		this.bestellingen.add(ph);
+	}
+	
+}
