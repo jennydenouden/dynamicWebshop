e7df854d2a52f81a7ccef8c543f052192e5e097e
Jenny <jenny.den.ouden@gmail.com>
2016-11-14 09:59:27 +0100

    Servlet voor de succespagina die nog niks doet

---
+++ src/webpagina/Succes.java
@@ -0,0 +1,25 @@
+package webpagina;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Succes
+ */
+@WebServlet("/Succes")
+public class Succes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
